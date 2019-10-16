fc49c121f4f7c1052a84d5e786f56408472a661b
tlqckd0 <tlqckd0@gmail.com>
2019-10-16 23:37:47 +0900

    asdfasdf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express  = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
+const path = require('path');
 require('dotenv').config();
 const app = express();
 
